f93b1051de2b6b09ed7a96fdbb34a97b18abde59
rica213 <25f5k252l@mozmail.com>
2023-02-19 17:39:58 +0300

    Define a function that display all countries

---
+++ src/modules/display-countries.js
@@ -0,0 +1,17 @@
+const displayCountries = (countries, container) => {
+  countries.forEach(country => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <img src="${country.flags.png}" alt="${country.flags.alt}" class="flag">
+      <div class="desc">
+        <p class="official-name">${country.name.official}</p>
+        <p class="population"><span>Population: </span>${country.population}</p>
+        <p class="region"><span>Region: </span>${country.region}</p>
+        <p class="capital"><span>Capital: </span>${country.capital}</p>
+      </div>
+    `;
+    container.appendChild(li);
+  });
+};
+
+export default displayCountries;
