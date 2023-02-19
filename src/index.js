ea99e5ff3ad4c919bfdad32d6c12dd728e017619
rica213 <25f5k252l@mozmail.com>
2023-02-19 17:41:01 +0300

    List all the countries on the homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import url from './modules/url.js';
+import retrieve from './modules/retrieve.js';
+import displayCountries from '.modules/display-countries.js';
+
+const container = document.querySelector('.countries ul');
+const result = retrieve(url);
+
+window.addEventListener('load', () => {
+  result.then(countries => {
+    displayCountries(countries, container);
+  });
+});
